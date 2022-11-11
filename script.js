5259a160f3f2d129a95e3e46cdebc6ef0687555d
Lester Lee <contact@lester-lee.com>
2022-11-11 13:39:26 -0500

    Write README with prompts

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Write your code here! */
+
+/**
+ * The code below exports your functions so they can be tested.
+ * Notice that the names are currently commented out.
+ * Uncomment the functions as you write them.
+ */
+module.exports = {
+  // multiplyNumber,
+  // vote,
+  // concatenateStrings,
+  // sumArray,
+  // sumToN,
+  // factorial,
+  // buildNArray,
+  // findMax,
+  // findLongestString,
+  // countPresent,
+  // getDnaComplement,
+  // isSongInPlaylist,
+  // isAllEven,
+  // makeBoard,
+  // evenAndOdd,
+  // exponentiate,
+  // onlyOdds,
+  // bacteriaTime,
+  // getAverage,
+  // countCoins,
+  // pairs,
+};
---
+++ script_test.js
@@ -0,0 +1,11 @@
+// --------------------------
+//#region Setup
+// You can safely ignore the code in this region :)
+// --------------------------
+// This line of code uses Chai, which is our assertion library.
+const { expect } = require("chai");
+
+// This imports your code so we can test it!
+const code = require("./script.js");
+//#endregion Setup
+ 
